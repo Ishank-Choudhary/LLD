abec75bb08e162fa54e4282b7694f62852141eb8
coachsahab <jaatishank@gmail.com>
2026-04-23 18:48:17 +0530

    Implementing singleton

--- src/CreationalPattern/Singleton/withSingleton/Main.java
+++ src/CreationalPattern/Singleton/withSingleton/Main.java
@@ -2,6 +2,12 @@ package CreationalPattern.Singleton.withSingleton;
 
 public class Main {
     public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        // both objects are same
+        System.out.println(singleton1==singleton2);
+
 
     }
 }
